4702a0941b101e6cac9edad32f9c2a1b4987d5b0
Claudio Yanes <me@claudio4.com>
2024-01-31 14:28:21 +0000

    Add wsl config

---
+++ roles/wsl/tasks/files/wsl-profile.sh
@@ -0,0 +1,11 @@
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/wsl2-ssh-pageant.sock"
+if ! ss -a | grep -q "$SSH_AUTH_SOCK"; then
+  rm -f "$SSH_AUTH_SOCK"
+  wsl2_ssh_pageant_bin="$HOME/.local/bin/wsl2-ssh-pageant.exe"
+  if test -x "$wsl2_ssh_pageant_bin"; then
+    (setsid nohup socat UNIX-LISTEN:"$SSH_AUTH_SOCK,fork" EXEC:"$wsl2_ssh_pageant_bin" >/dev/null 2>&1 &)
+  else
+    echo >&2 "WARNING: $wsl2_ssh_pageant_bin is not executable."
+  fi
+  unset wsl2_ssh_pageant_bin
+fi
