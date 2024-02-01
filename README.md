6ecae2068a8a33e4617cd39af9b601123cadf334
Claudio Yanes <me@claudio4.com>
2024-02-01 15:43:34 +0000

    Improve bootstrap script

--- README.md
+++ README.md
@@ -8,10 +8,16 @@ ansible-pull -U https://github.com/claudio4/dotfiles.git --ask-become-pass
 ```
 
 If you are in a completely bare system, you can use the `bootstrap.sh` script.
-```
-# You can also use https://cl4.es/env.sh for less typing, it redirects to the same file.
-bash -c 'eval $(wget -O- https://raw.githubusercontent.com/claudio4/dotfiles/master/bootstrap.sh)'
+```bash
+# If you are in an interactive shell, you will be asked for tags.
+# In that note, the eval is important to keep the interactivity.
+# BTW, cl4.es/env.sh redirects to the bootstrap script in this repository, you
+# can use the raw github link directly if you prefer.
+eval "$(wget -qO- https://cl4.es/env.sh)"
 
-# You can also set tags
-TAGS="server lazygit" bash -c 'eval "$(wget -O- https://cl4.es/env.sh)'
+# You can also set tags before hand
+TAGS="server lazygit" eval "$(wget -qO- https://cl4.es/env.sh)"
+
+# If you are setting the flags before hand you might not need an interactive shell so you can do
+TAGS="server lazygit" wget -qO- https://raw.githubusercontent.com/claudio4/dotfiles/master/install.sh | bash
 ```
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,65 +1,18 @@
-#!/bin/bash
-set -e
-
-
-
-install_package() {
-    if ! command -v sudo &> /dev/null; then
-        echo "sudo is not installed. Please manually install $1."
-        exit 1
-    fi
-
-
-    if command -v zypper &> /dev/null; then
-        sudo zypper install "$1" -y
-    elif command -v apt &> /dev/null; then
-        sudo apt install "$1" -y
-    elif command -v dnf &> /dev/null; then
-        sudo dnf install "$1" -y
-    elif command -v pacman &> /dev/null; then
-        sudo pacman -Sy "$1" --noconfirm
-    else
-        echo "Unable to install package. No supported package manager found."
-        exit 1
-    fi
-}
-
-
-
-
-# Check if Ansible is installed
-if ! command -v ansible &> /dev/null; then
-   install_package ansible
-fi
-
-# Check if git is installed
-if ! command -v git &> /dev/null; then
-   install_package git
-fi
-
-
-if [ -z $PS1 ]; then
-    echo "About to run ansible-pull -U https://github.com/claudio4/dotfiles.git --ask-become-pass $@"
-    read -p "Continue? (y/n) " -n 1 -r
-    echo    # move to a new line
-    if [[ ! $REPLY =~ ^[Yy]$ ]]
-    then
-        exit 1
-    fi
-    ask_pass="--ask-become-pass"
+# Check if curl or wget is available
+if command -v curl >/dev/null 2>&1; then
+    DOWNLOADER="curl -fssL"
+elif command -v wget >/dev/null 2>&1; then
+    DOWNLOADER="wget -qO-"
 else
-    echo "Running ansible-pull -U https://github.com/claudio4/dotfiles.git \"$@\""
-    ask_pass=""
+    echo "Please install either curl or wget to make HTTP request."
+    exit 1
 fi
 
-if [[ -n $TAGS ]]; then
-    # Split the TAGS variable by spaces and prepend each tag with -t
-    IFS=' ' read -ra ADDR <<< "$TAGS"
-    tags=""
-    for i in "${ADDR[@]}"; do
-        tags+=" -t $i"
-    done
+# Prompt the user for tags if running interactively and there is no tags defined.
+if [ -t 0 ] && [ -z "$TAGS" ]; then
+    echo "Enter tags (separated by spaces) (ej. workstation server):"
+    read -r TAGS
+    export TAGS="$TAGS"
 fi
 
-# Run Ansible playbook using ansible-pull
-ansible-pull -U https://github.com/claudio4/dotfiles.git $ask_pass $tags
+bash -c "$($DOWNLOADER https://raw.githubusercontent.com/claudio4/dotfiles/master/install.sh)"
