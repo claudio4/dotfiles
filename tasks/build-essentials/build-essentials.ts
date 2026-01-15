ea1278ce66f37ec7bd1a107ddb4a47e855034ede
Claudio Yanes <me@claudio4.com>
2026-01-15 02:19:55 +0000

    Add install build-essentials task

---
+++ tasks/build-essentials/build-essentials.ts
@@ -0,0 +1,31 @@
+import { install, refresh } from "internal/package-manager";
+import type { PackageDefinition } from "internal/package-manager/manager";
+import { BaseTask } from "internal/task";
+
+const buildPackages: PackageDefinition[] = [
+  {
+    apt: "build-essential",
+    dnf: "@development-tools",
+    zypper: "pattern:devel_basis",
+    pacman: "base-devel",
+    default: "build-essential",
+  },
+  {
+    default: "procps",
+    dnf: "procps-ng",
+    pacman: "procps-ng",
+  },
+  "git",
+  "curl",
+  "file",
+];
+
+class BuildEssentialsTask extends BaseTask {
+  override id = "BuildEssentials";
+  override _execute(): Promise<void> {
+    this.setMessage("Installing build essentials...");
+    return install(buildPackages);
+  }
+}
+
+export default new BuildEssentialsTask();
