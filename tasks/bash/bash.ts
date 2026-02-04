32aca099742d2c6f287c50fd5c53491943ba0edb
Claudio Yanes <me@claudio4.com>
2026-02-04 00:02:00 +0000

    Add modern utils to bash

--- tasks/bash/bash.ts
+++ tasks/bash/bash.ts
@@ -1,8 +1,9 @@
-import { which } from "internal/cmd";
+import { commandExists, which } from "internal/cmd";
 import { BaseTask, getTask, isTaskRegistered, TaskStatus } from "internal/task";
 import { compileTemplateFromFile } from "internal/template";
 import { getConfigHome, getHome } from "internal/user";
 import { isWSL } from "internal/utils";
+import { get } from "node:https";
 import { join } from "node:path";
 
 class BashTask extends BaseTask {
@@ -12,14 +13,24 @@ class BashTask extends BaseTask {
     interactiveToFish: true,
   };
   override async _execute(): Promise<void> {
+    const moderUtils = await isTaskRegistered("modern-utils");
+
     const vars: {
+      bat: boolean;
       configHome: string;
+      eza: boolean;
       fish?: string;
+      fzf: boolean;
       homebrew?: string;
       wsl: boolean;
+      zoxide: boolean;
     } = {
       configHome: getConfigHome(),
       wsl: isWSL(),
+      bat: commandExists("bat") || moderUtils,
+      eza: commandExists("eza") || moderUtils,
+      fzf: commandExists("fzf") || moderUtils,
+      zoxide: commandExists("zoxide") || moderUtils,
     };
     if (this.options.interactiveToFish) {
       const fishPath = which("fish");
@@ -32,6 +43,7 @@ class BashTask extends BaseTask {
     if (homebrew && homebrew.getInfo().status !== TaskStatus.Unregistered) {
       vars.homebrew = homebrew.options!.homebrewPath;
     }
+
     this.setMessage("Create .bashrc");
     const bashrc = (await compileTemplateFromFile(join(import.meta.dir, "bashrc.tmpl")))(vars);
     await Bun.file(join(getHome(), ".bashrc")).write(bashrc);
