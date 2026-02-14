de0d6d9e60641cbb3567b56f6cf6ea151b541bc4
Claudio Yanes <me@claudio4.com>
2026-02-14 05:19:49 +0000

    Add profile for my laptop

---
+++ profiles/machine-envycl4.ts
@@ -0,0 +1,44 @@
+import { Profile } from "internal/profile";
+import { getDistroFamily } from "internal/utils";
+import bash from "tasks/bash/bash";
+import basicTools from "tasks/basic-tools/basic-tools";
+import braveBrowser from "tasks/brave-browser/brave-browser";
+import buildEssentials from "tasks/build-essentials/build-essentials";
+import fish from "tasks/fish/fish";
+import git from "tasks/git/git";
+import go from "tasks/go/go";
+import homebrew from "tasks/homebrew/homebrew";
+import modernUtils from "tasks/modern-utils/modern-utils";
+import neovim from "tasks/neovim/neovim";
+import rust from "tasks/rust/rust";
+import tmux from "tasks/tmux/tmux";
+import wsl from "tasks/wsl/wsl";
+import zed from "tasks/zed/zed";
+import zsh from "tasks/zsh/zsh";
+
+const profile = new Profile({
+  name: "EnvyCl4",
+  description: "Profile for my Linux Laptop",
+});
+
+// Arch has all package I need in its repos and by using those we avoid having to compile
+//  some  homebrew packages.
+homebrew.options.shouldOverride = (await getDistroFamily()) !== "arch";
+
+profile.addTask(
+  git,
+  homebrew,
+  buildEssentials,
+  bash,
+  basicTools,
+  fish,
+  go,
+  neovim,
+  zed,
+  modernUtils,
+  tmux,
+  zsh,
+  braveBrowser,
+);
+
+export default profile;
